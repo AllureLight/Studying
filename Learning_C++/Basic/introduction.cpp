7a65919ba92e5532213f3fd6b074c0ed73c65cc2
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-03-02 23:20:28 -0300

    in

--- Learning_C++/Basic/introduction.cpp
+++ Learning_C++/Basic/introduction.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 
 typedef std::vector<int> IntVector; //typedef is used to create an alias for a type
+typedef std::string text_t;
+using number_t = int; //using is another way to create an alias for a type, it is more modern and preferred in C++11 and later
 
 namespace firts{
     int x = 2;
@@ -32,5 +34,15 @@ int main() {
     std::cout << "\nValue of x in first: " << firts::x; //accessing variable x from namespace first
 
     //TYPEDEF
+    //typedef is used to create an alias for a type, it can be used to make code more readable and easier to maintain
+    text_t firtname = "John"; //using typedef to declare a string variable
+    //number_t age = 30; //using using to declare an integer variable
+
+    //ARTHIMETIC OPERATORS
+    // + - * / % are the basic arithmetic operators in C++
+
+    //TYPE CONVERSION
+    double example = (int) 3.14; //type casting double to int, it will truncate the decimal part and pi will be 3
+    std::cout << "\nValue of example after type conversion: " << example;
     return 0;
 }
