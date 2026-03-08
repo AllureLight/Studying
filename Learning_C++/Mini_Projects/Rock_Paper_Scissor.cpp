526ffbafbdbad5a6731d2a3673a4dad39f0605b5
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-03-08 14:53:41 -0300

    rock

--- Learning_C++/Mini_Projects/Rock_Paper_Scissor.cpp
+++ Learning_C++/Mini_Projects/Rock_Paper_Scissor.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+char getComputerChoice();
+
 int main(){
+    char playerChoice;
+
+    do{
+        std::cout << "Enter your choice (R, P, S): ";
+        std::cin >> playerChoice;
+    }while(playerChoice != 'R' && playerChoice != 'P' && playerChoice != 'S');
+
+    return 0;
+}
+
+char getComputerChoice(){
     
 }
