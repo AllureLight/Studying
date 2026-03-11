93cea9857972d9c3c30f40aecb9acfc61ed44579
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-03-10 23:23:54 -0300

    10 ajustated

--- Learning_C++/Lessons/March/10.cpp
+++ Learning_C++/Lessons/March/10.cpp
@@ -8,10 +8,8 @@
 
 std::pair<std::size_t, std::size_t> two_sum(const std::vector<int>& numbers, int target) {
   for(std::size_t i = 0; i < numbers.size(); i++){
-      for(std::size_t j = 0; j < numbers.size(); j++){
-        if(j != i && numbers[i] + numbers[j] == target){
-            return {i, j};
-        }
+      for(std::size_t j = i+1; j < numbers.size(); j++){
+        if(numbers[i] + numbers[j] == target) return {i, j};
       }
   }
     return {0, 0};
