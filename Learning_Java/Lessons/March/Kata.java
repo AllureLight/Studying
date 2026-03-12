7125c57305b1b7b283fcc4c036026e9b86a51735
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-03-11 22:29:40 -0300

    10

---
+++ Learning_Java/Lessons/March/Kata.java
@@ -0,0 +1,12 @@
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+public class Kata {
+  public static int[] digitize(long n) {
+    String num = Long.toString(n);
+    int[] vetor = new int[num.length()];
+    for(int i = 0; i < num.length(); i++){
+      vetor[i] = num.charAt(num.length() - 1 - i) - '0';
+    }
+      return vetor;
+  }
+}
