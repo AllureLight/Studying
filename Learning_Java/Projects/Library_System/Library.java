b1ea27316f3e0643cf5059abaf7c0f8fc7d035e7
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-03-12 22:57:31 -0300

    projects2

--- Learning_Java/Projects/Library_System/Library.java
+++ Learning_Java/Projects/Library_System/Library.java
@@ -3,22 +3,37 @@ package Learning_Java.Projects.Library_System;
 import java.util.ArrayList;
 
 public class Library {
-    ArrayList<Book> books = new ArrayList<>();
+    private ArrayList<Book> books = new ArrayList<>();
 
-    void addBook(String bookName) {
-        books.add(new Book());
-        System.out.println("Book added: " + bookName);
+    public void addBook(String title, String author) {
+        books.add(new Book(title, author));
+        System.out.println("Book added: " + title);
     }
 
-    void removeBook(Book book) {
-        books.remove(book);
-        System.out.println("Book removed: " + book.getTitle());
+    public Book searchBook(String tittle) {
+        for(Book book : books) {
+            if(book.getTitle().equals(tittle)) {
+                return book;
+            }
+        }
+        return null;
     }
 
-    String[] searchBook(String keyword) {
-        String[] books = {"Java Programming", "Data Structures", "Algorithms"};
-        return books;
+    public void removeBookByTittle(String tittle) {
+        Book bookToRemove = searchBook(tittle);
+        
+        if(bookToRemove != null) {
+            books.remove(bookToRemove);
+            System.out.println("Book removed: " + bookToRemove.getTitle());
+        }
+        else {
+            System.out.println("Book not found: " + tittle);
+        }
     }
 
-
+    public void ListBooks() {
+        for(Book book : books){
+            System.out.println("Book: " + book.getTitle() + " by " + book.getAuthor());
+        }
+    }
 }
--- Learning_Java/Projects/Library_System/Main.java
+++ Learning_Java/Projects/Library_System/Main.java
@@ -1,10 +1,28 @@
 package Learning_Java.Projects.Library_System;
 
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
+
+        int option;
+        do{
+            System.out.println("Library System");
+            System.out.println("1. Add Book");
+            System.out.println("2. List Books");
+            System.out.println("3. Borrow Book");
+            System.out.println("4. Return Book");
+            System.out.println("5. Exit");
+            option = scanner.nextInt();
+
+            switch(option){
+                
+            }
+        }while(option != 5);
+
+        scanner.close();
     }
 
 }
